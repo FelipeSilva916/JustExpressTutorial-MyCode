0097aaae66ea2dc460a2203c429a6ce422dd5f67
FelipeSilva916 <87789520+FelipeSilva916@users.noreply.github.com>
2022-04-07 12:29:23 -0700

    all handler

--- express101/expressServer.js
+++ express101/expressServer.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+//All is  a method, it takes 2 args:
+//  1. Route
+//  2. Callbacl to runif the route is requested
+
+app.all("*", (req, res) => {
+  //express handles the basic headers(status code, myme-type)
+});
