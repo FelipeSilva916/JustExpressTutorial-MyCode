8a7aa804ee99cb50e956e6178c92175cb7d38441
Felipe Silva <felipesilva916@gmail.com>
2022-05-04 17:27:20 -0700

    fixed error

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -6,11 +6,11 @@ const apiKey = "1fb720b97cc13e580c2c35e1138f90f8";
 // const apiKey = '123456789';
 const apiBaseUrl = "http://api.themoviedb.org/3";
 // const apiBaseUrl = 'http://localhost:3030';
-const nowPlayingUrl = `${apiBaseUrl}/most_popular?api_key=${apiKey}`;
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
 
 router.use((req, res, next) => {
-  res.locals.imageBaseUrl - imageBaseUrl;
+  res.locals.imageBaseUrl = imageBaseUrl;
   next();
 });
 
@@ -20,7 +20,7 @@ router.get("/", function (req, res, next) {
     const parsedData = JSON.parse(movieData);
 
     res.render("index", {
-      parsedData: parsedData.results,
+      parsedData: parsedData.results
     });
   });
 });
