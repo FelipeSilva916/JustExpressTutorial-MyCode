4c629297e6d150c6dc24346800c1c5f4b464f234
Felipe Silva <felipesilva916@gmail.com>
2022-05-08 12:40:33 -0700

    Video 44

--- movieApi/app.js
+++ movieApi/app.js
@@ -5,8 +5,12 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const helmet = require("helmet");
 
+//now playing
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+//movies
+var movieRouter = require("./routes/movie");
+//search
+const searchRouter = require("./routes/search");
 
 var app = express();
 app.use(helmet());
@@ -22,7 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/movie", movieRouter);
+app.use("/search", searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
