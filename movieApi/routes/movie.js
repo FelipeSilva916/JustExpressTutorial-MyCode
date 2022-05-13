14c29b4c880b6fbbf70702b600e94ff371287b5e
Felipe Silva <felipesilva916@gmail.com>
2022-05-13 01:08:29 -0700

    Video 47

--- movieApi/routes/movie.js
+++ movieApi/routes/movie.js
@@ -2,12 +2,35 @@ var express = require("express");
 var router = express.Router();
 const movieDetails = require("../data/movieDetails");
 
+function requireJSON(req, res, next) {
+  if (!req.is("application/json")) {
+    res.json({ msg: "Content type must be application/json" });
+  } else {
+    next();
+  }
+}
+
 //all routes here have /movie in front
 router.get("/", function (req, res, next) {
   res.json("MOVIE page goes here");
 });
+// =========================//
 
-//GET /movie/movieID
+// ========  GET movie/top_rated ==============//
+router.get("/top_rated", (req, res) => {
+  let page = req.query.page;
+  if (!page) {
+    page = 1;
+  }
+  const results = movieDetails.sort((movie1, movie2) => {
+    return movie2.vote_average - movie1.vote_average;
+  });
+  let startingIndex = (page - 1) * 20;
+  res.json(results.slice(startingIndex, startingIndex + 20));
+});
+//========================//
+
+//============= GET /movie/movieID ================//
 router.get("/:movieId", (req, res) => {
   const movieId = req.params.movieId;
   const result = movieDetails.find((movie) => {
@@ -24,9 +47,25 @@ router.get("/:movieId", (req, res) => {
   }
 });
 
-//GET movie/top_rated
-
-//POST /movie/movieId/rating
+//=============  POST /movie/movieId/rating ==============
+router.post("/:movieId/rating", requireJSON, (req, res, next) => {
+  const { movieId } = req.params;
+  const userRating = req.body.value;
+  if (userRating < 0.5 || userRating > 10) {
+    res.json({
+      msg: "Rating must be between 1 and 10"
+    });
+  } else {
+    res.json({
+      msg: "Thank you for submitting your rating",
+      status_code: 200
+    });
+  }
+});
 
 //DELETE //movie/movieId/rating
+router.delete("/:movieId/rating", requireJSON, (req, res, next) => {
+  res.json({ msg: "Rating deleted!" });
+});
+
 module.exports = router;
